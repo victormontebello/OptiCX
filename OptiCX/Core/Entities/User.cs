7576bc8ad8e0f20f23add2ccf00375257796f394
montebello <victor.montebello@inventsoftware.com.br>
2025-01-07 07:39:22 -0300

    initializing user

---
+++ OptiCX/Core/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities;
+
+public class User
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Token { get; set; }
+}
