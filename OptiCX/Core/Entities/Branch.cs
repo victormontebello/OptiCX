8bc90daaf7363e097b4a05f5017d55a1f1ef72ad
montebello <montebellovictor@gmail.com>
2025-01-08 11:24:42 -0300

    branch ids

--- OptiCX/Core/Entities/Branch.cs
+++ OptiCX/Core/Entities/Branch.cs
@@ -3,6 +3,7 @@
 public class Branch
 {
     public int Id { get; set; }
+    public int CompanyId { get; set; }
     public string Name { get; set; }
     public Address Address { get; set; }
     public string Email { get; set; }
