5208342f038160ca278bd87b3f04587c488d7e45
victormontebello <montebello@discente.ufg.br>
2025-01-27 22:17:15 -0300

    base view

---
+++ OptiCX/Application/wwwroot/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, FooterComponent, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'wwwroot';
+}
