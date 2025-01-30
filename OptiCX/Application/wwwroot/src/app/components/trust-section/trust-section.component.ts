c6b9816b3854c08922231990f9855d8b26fdb6c9
montebello <montebellovictor@gmail.com>
2025-01-30 19:12:08 -0300

    finalizing home

--- OptiCX/Application/wwwroot/src/app/components/trust-section/trust-section.component.ts
+++ OptiCX/Application/wwwroot/src/app/components/trust-section/trust-section.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { Feature } from '../../interfaces/feature';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-trust-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './trust-section.component.html',
   styleUrl: './trust-section.component.css'
 })
 export class TrustSectionComponent {
-
+  features: Feature[] = [
+    {
+      icon: "👥",
+      title: "Customer Insights",
+      description: "Gain deep understanding of your customers' needs and behaviors.",
+    },
+    {
+      icon: "📊",
+      title: "Analytics Dashboard",
+      description: "Visualize key metrics and trends to make data-driven decisions.",
+    },
+    {
+      icon: "💬",
+      title: "Omnichannel Support",
+      description: "Provide seamless support across multiple communication channels.",
+    },
+    {
+      icon: "⚡",
+      title: "AI-Powered Automation",
+      description: "Streamline processes and enhance efficiency with intelligent automation.",
+    },
+  ]
 }
